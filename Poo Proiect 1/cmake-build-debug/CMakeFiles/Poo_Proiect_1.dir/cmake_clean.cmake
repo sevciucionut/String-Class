b8245345ed3e466e53712f9e3a1163980e355089
Sevciuc <sevciucionut@yahoo.com>
2019-03-18 14:41:48 +0200

    Poo Proiect 1

---
+++ Poo Proiect 1/cmake-build-debug/CMakeFiles/Poo_Proiect_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poo_Proiect_1.dir/main.cpp.obj"
+  "Poo_Proiect_1.pdb"
+  "Poo_Proiect_1.exe"
+  "Poo_Proiect_1.exe.manifest"
+  "libPoo_Proiect_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Poo_Proiect_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Poo Proiect 1/cmake-build-debug/CMakeFiles/Poo_Proiect_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Poo_Proiect_1.
+# This may be replaced when dependencies are built.
